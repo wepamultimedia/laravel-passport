5e05d688cb4dae787608db2269c242f9d8a0eba1
Carlos Puche <cpuche@wepa.es>
2022-03-28 15:37:36 +0100

    Test route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@
 | Here is where you can register routes for your package.
 |
 */
+Route::get('/auth', function(){
+	return 'Test auth';
+});
--- src/Providers/LaravelPassportApiServiceProvider.php
+++ src/Providers/LaravelPassportApiServiceProvider.php
@@ -28,7 +28,7 @@ class LaravelPassportApiServiceProvider extends ServiceProvider
          * Uncomment this function call to load the route files.
          * A web.php file has already been generated.
          */
-        // $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
+         $this->loadRoutesFrom(__DIR__.'/../../routes/web.php');
 
         /**
          * Translations
